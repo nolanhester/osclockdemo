7936f44eb159a4a19b09b96903230de8325fb50a
sfiero1 <crytekmax@Sams-MacBook-Air.local>
2013-10-13 16:17:12 -0400

    I

--- systemClock.java
+++ systemClock.java
@@ -3,18 +3,25 @@ public class systemClock
     public static void main(String[] args)
         throws InterruptedException 
         {
-            long timerStart = System.currentTimeMillis();
-            Thread.sleep(4000);
+            
             long timerCheck = System.currentTimeMillis() - timerStart;
             double elapsedSec = timerCheck / 1000;
             System.out.println(elapsedSec); 
     }
 }
-class SystemClock
+class DemoMethods
 {
-	public void clock()
+	public void startClock()
+	{
+		long startTime = System.currentTimeMillis(); //start time
+		
+	}
+	
+	public double getCurrentTime()
 	{
 		
 	}
+	
+	public 
 }
 //comment
