a9b0dfda22c8097b49f21938777f1766a76bafc0
Nolan Hester <nephreum@gmail.com>
2013-10-13 14:42:02 -0400

    init commit

---
+++ systemClock.java
@@ -0,0 +1,12 @@
+public class systemClock
+{
+    public static void main(String[] args)
+        throws InterruptedException 
+        {
+            long timerStart = System.currentTimeMillis();
+            Thread.sleep(4000);
+            long timerCheck = System.currentTimeMillis() - timerStart;
+            double elapsedSec = timerCheck / 1000;
+            System.out.println(elapsedSec); 
+    }
+}
